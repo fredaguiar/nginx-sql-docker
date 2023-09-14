934634923e1fa281287a461b59f53f20d703f5ee
fredaguiar <fredaguiar@FTeixeira>
2023-09-13 22:12:36 -0700

    style components

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-readme
+# Dockerized Nginx React Application wioth NodeJS and MySql
+
+### Start
+
+    Go to the root of the project nginx-sql and run docker compose:
+
+    /nginx-sql$ docker-compose up --build
+
+    a new docker container will be created and run (nginx-sql)
+
+### MySql admin
+
+    http://localhost:8000/
+    Username: root
+    Password: root
+    Database: books
+
+### UI
+
+    http://localhost:3050/
--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,50 @@
 import { useState, Fragment } from "react";
 import "./App.css";
 import axios from "axios";
-import { Container, Col, Row } from "react-bootstrap";
+import styled from "styled-components";
 
+const Title = styled.h1`
+  margin-left: 200px;
+  margin-bottom: 20px;
+  color: black;
+`;
+
+const TestBackend = styled.div`
+  margin-left: 200px;
+  color: black;
+`;
+
+const Spacer50 = styled.div`
+  margin-bottom: 50px;
+`;
+
+const Row = styled.div`
+  display: flex;
+  padding: 5px;
+  margin-left: 200px;
+  width: 310px;
+  background-color: #cccccc;
+`;
+
+const COL1 = styled.div`
+  width: 100px;
+  justify-content: flex-start;
+`;
+
+const COL2 = styled.div`
+  width: 200px;
+  justify-content: flex-start;
+`;
+
+const RowItems = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 2px;
+  margin-left: 200px;
+  width: 410px;
+  background-color: #cccccc;
+  border-bottom: 1px solid black;
+`;
 const App = () => {
   const [book, setBook] = useState("");
   const [review, setReview] = useState("");
@@ -45,35 +87,66 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>Dockerized Fullstack React Application</h1>
-      <input type="button" value="Test Backend" onClick={test} />
+      <Title>Dockerized Fullstack React Application</Title>
+      <TestBackend>
+        <input type="button" value="Test Backend" onClick={test} />
+      </TestBackend>
+      <Spacer50 />
       <form onSubmit={handleSubmit}>
-        <div className="form">
+        <Row>
+          <COL1>
+            <label htmlFor="book">Book tile:</label>
+          </COL1>
+          <COL2>
+            <input
+              name="book"
+              type="text"
+              value={book}
+              onChange={(e) => setBook(e.target.value)}
+            />
+          </COL2>
+        </Row>
+        <Row>
+          <COL1>
+            <label htmlFor="review">Review:</label>
+          </COL1>
+          <COL2>
+            <input
+              name="review"
+              type="text"
+              value={review}
+              onChange={(e) => setReview(e.target.value)}
+            />
+          </COL2>
+        </Row>
+        <Row>
+          <COL1></COL1>
+          <COL2>
+            <input type="submit" />
+          </COL2>
+        </Row>
+        <Spacer50 />
+
+        <RowItems>
           <input
-            type="text"
-            value={book}
-            onChange={(e) => setBook(e.target.value)}
+            type="button"
+            style={{ width: "150px" }}
+            value="Get all books"
+            onClick={getBooks}
           />
-          <input
-            type="text"
-            value={review}
-            onChange={(e) => setReview(e.target.value)}
-          />
-        </div>
-        <input type="submit" />
-        <Container>
-          <Row>
-            <Col>
-              <input type="button" value="Get all books" onClick={getBooks} />
-            </Col>
-          </Row>
-          {allBooks &&
-            allBooks.map((val) => (
-              <Row>
-                <Col>{val.book_name}</Col>
-              </Row>
-            ))}
-        </Container>
+        </RowItems>
+
+        {allBooks &&
+          allBooks.map((val, index) => (
+            <RowItems key={index}>
+              <div>
+                <strong>Title:</strong> {val.book_review}
+              </div>
+              <div>
+                <strong>Review:</strong> {val.book_name}
+              </div>
+            </RowItems>
+          ))}
       </form>
     </div>
   );
